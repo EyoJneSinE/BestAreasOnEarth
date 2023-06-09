1d6788917bbb998831bddf1d00afc2bf5dc966cf
Enis <enis.d.kaner@gmail.com>
2023-06-09 14:45:28 +0300

    AreaDetailsFragment added and binded xml

---
+++ app/src/main/java/com/eniskaner/bestareasonearth/view/AreaDetailsFragment.kt
@@ -0,0 +1,13 @@
+package com.eniskaner.bestareasonearth.view
+
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.Fragment
+import com.eniskaner.bestareasonearth.R
+
+class AreaDetailsFragment: Fragment(R.layout.fragment_area_details) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
