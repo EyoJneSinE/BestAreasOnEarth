e5b7627f5d3ec9950f1a3766e2c25adeffe9de7d
Enis <enis.d.kaner@gmail.com>
2023-06-09 14:47:06 +0300

    ImageApiFragment added and binded xml

---
+++ app/src/main/java/com/eniskaner/bestareasonearth/view/ImageApiFragment.kt
@@ -0,0 +1,13 @@
+package com.eniskaner.bestareasonearth.view
+
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.Fragment
+import com.eniskaner.bestareasonearth.R
+
+class ImageApiFragment: Fragment(R.layout.fragment_image_api) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
