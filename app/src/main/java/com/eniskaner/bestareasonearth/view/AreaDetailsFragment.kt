b730c41eee91458308c7a619e99cbb429372c558
Enis <enis.d.kaner@gmail.com>
2023-06-09 19:25:42 +0300

    navigates done with onclicklisteners

--- app/src/main/java/com/eniskaner/bestareasonearth/view/AreaDetailsFragment.kt
+++ app/src/main/java/com/eniskaner/bestareasonearth/view/AreaDetailsFragment.kt
@@ -3,11 +3,23 @@ package com.eniskaner.bestareasonearth.view
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.eniskaner.bestareasonearth.R
+import com.eniskaner.bestareasonearth.databinding.FragmentAreaDetailsBinding
 
 class AreaDetailsFragment: Fragment(R.layout.fragment_area_details) {
 
+    private var fragmentAreaDetailsBinding : FragmentAreaDetailsBinding? = null
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        val binding = FragmentAreaDetailsBinding.bind(view)
+        fragmentAreaDetailsBinding = binding
+
+
+        binding.areaImageView.setOnClickListener {
+            findNavController().navigate(AreaDetailsFragmentDirections.actionAreaDetailsFragmentToImageApiFragment())
+        }
     }
 }
--- app/src/main/java/com/eniskaner/bestareasonearth/view/AreaFragment.kt
+++ app/src/main/java/com/eniskaner/bestareasonearth/view/AreaFragment.kt
@@ -3,11 +3,25 @@ package com.eniskaner.bestareasonearth.view
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.eniskaner.bestareasonearth.R
+import com.eniskaner.bestareasonearth.databinding.FragmentAreasBinding
 
 class AreaFragment: Fragment(R.layout.fragment_areas) {
 
+    private var fragmentBinding : FragmentAreasBinding? = null
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        var binding = FragmentAreasBinding.bind(view)
+        fragmentBinding = binding
+
+        binding.fabButton.setOnClickListener {
+            findNavController().navigate(AreaFragmentDirections.actionAreaFragmentToAreaDetailsFragment())
+        }
+    }
+    override fun onDestroyView() {
+        fragmentBinding = null
+        super.onDestroyView()
     }
 }
